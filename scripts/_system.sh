05c064ff8e5a47c7b789716768d42609724c469f
phil <pgleghorn@gmail.com>
2016-12-05 16:13:49 +0000

    parameterising directories to ensure in _system.sh

--- scripts/_system.sh
+++ scripts/_system.sh
@@ -1,4 +1,4 @@
-#!/bin/sh -a
+#!/bin/sh -ax
 
 . $V_CONFIG
 echo system
@@ -12,7 +12,8 @@ chkconfig --level 345 ip6tables off 2>/dev/null
 usermod -p '$1$aUtH9gPt$/ykXsfv.w52tq6FlBIQZC0' root # pass1234
 useradd -d $V_UNIXUSERHOME -m -p '$1$aUtH9gPt$/ykXsfv.w52tq6FlBIQZC0' $V_UNIXUSER # pass1234
 # ensure paths exist
-for i in $V_UNIXUSERHOME $V_INSTALLDIR $V_SITES_INSTALLDIR $V_SITES_SHAREDDIR $V_TOMCAT_INSTALLDIR $V_JDK_INSTALLDIR; do
+eval dirs="\$$1"
+for i in $dirs; do
 	mkdir -p $i
 	chown $V_UNIXUSER:$V_UNIXGROUP $i
 done
