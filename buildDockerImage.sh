80fbb966000f1173984587f58a21975e808b4599
phil <pgleghorn@gmail.com>
2016-12-05 11:57:32 +0000

    renamed docker images from vs to wcs

--- buildDockerImage.sh
+++ buildDockerImage.sh
@@ -8,7 +8,7 @@ if [ -z $ORACLE_USER ] &&  [ -z $ORACLE_PASSWORD ]; then
 fi
 downloadargs=" --build-arg ORACLE_USER=$ORACLE_USER --build-arg ORACLE_PASSWORD=$ORACLE_PASSWORD"
 vargs=" --build-arg V_SCRIPTS=$V_SCRIPTS --build-arg V_CONFIG=$V_CONFIG "
-docker build $vargs $downloadargs -t vsbase $@ -f Dockerfile-base .
-docker build $vargs -t vsinstall $@ -f Dockerfile-install .
-docker build $vargs -t vspatch $@ -f Dockerfile-patch .
-docker build $vargs -t vsrun $@ -f Dockerfile-run .
+docker build $vargs $downloadargs -t wcsbase $@ -f Dockerfile-wcsbase .
+docker build $vargs -t wcsinstall $@ -f Dockerfile-wcsinstall .
+docker build $vargs -t wcspatch $@ -f Dockerfile-wcspatch .
+docker build $vargs -t wcsrun $@ -f Dockerfile-wcsrun .
