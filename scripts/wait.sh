c3d1791bbaddfb6829954052eb22d3e9a379115c
phil <pgleghorn@gmail.com>
2016-12-23 00:47:55 +0000

    parameterise file to watch in wait.sh

--- scripts/wait.sh
+++ scripts/wait.sh
@@ -1,8 +1,10 @@
 #!/bin/sh
 
+FILE=$1
+
 # wait until sites install log asks us to restart tomcat
 TARGET="^4. If the test is successful, press ENTER.$"
-FILE=$V_SITES_INSTALLDIR/ominstallinfo/install_log.log
+#FILE=$V_SITES_INSTALLDIR/ominstallinfo/install_log.log
 while true; do
   grep -q $TARGET $FILE 2>/dev/null
   if [ $? -eq 0 ]; then
