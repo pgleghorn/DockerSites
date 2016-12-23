8f2c2afaaaaeb89ea401fc4543611e03e6fecf7e
phil <pgleghorn@gmail.com>
2016-12-23 11:18:45 +0000

    removed startupDynamic.sh

--- scripts/startupDynamic.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh -a
-
-echo startupDynamic
-V_NEW_COMPOSE_HOSTNAME=${1:?new compose hostname}
-V_NEW_COMPOSE_PORT=${2:?new compose port}
-. $V_CONFIG
-env | grep V_ | sort
-rename.sh $V_SITES_INSTALLDIR $V_TOMCAT_INSTALLDIR
-catalina.sh run
